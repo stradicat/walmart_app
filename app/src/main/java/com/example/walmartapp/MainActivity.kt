1a84e01e3b76f54af17baf6a79e1db519fbbd3d3
Dennis Mayr <dmayr.dev@gmail.com>
2024-05-20 21:22:39 -0400

    drawable: agregado logo de app "diler" + paleta de colores inicial

--- app/src/main/java/com/example/walmartapp/MainActivity.kt
+++ app/src/main/java/com/example/walmartapp/MainActivity.kt
@@ -1,20 +1,16 @@
 package com.example.walmartapp
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.walmartapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        val view = binding.root
+        setContentView(view)
     }
 }
