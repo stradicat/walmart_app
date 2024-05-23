57023edb91b6db46a96ac95554fbf66ab6cf043c
Dennis Mayr <dmayr.dev@gmail.com>
2024-05-23 02:23:10 -0400

    Added `domain` layer with dummy product data

---
+++ app/src/main/java/com/example/walmartapp/domain/DummyData.kt
@@ -0,0 +1,68 @@
+package com.example.walmartapp.domain
+
+import com.example.walmartapp.R
+
+class DummyData {
+    val fakeProducts = mutableListOf(
+        DummyProduct(
+            "Nescafe Tradición 150g",
+            5590,
+            drawableResId = R.drawable.img_placement_nescafe
+        ),
+        DummyProduct(
+            "Papel Higiénico 50m 12u",
+            10490,
+            drawableResId = R.drawable.img_placement_papelhigienico
+        ),
+        DummyProduct(
+            "Queso laminado Quilque 250g",
+            2990,
+            drawableResId = R.drawable.img_placement_queso
+        ),
+        DummyProduct(
+            "Nescafe Tradición 151g",
+            6590,
+            drawableResId = R.drawable.img_placement_nescafe
+        ),
+        DummyProduct(
+            "Papel Higiénico 50.5m 12u",
+            11490,
+            drawableResId = R.drawable.img_placement_papelhigienico
+        ),
+        DummyProduct(
+            "Queso laminado Quilque 249g",
+            3500,
+            drawableResId = R.drawable.img_placement_queso
+        ),
+        DummyProduct(
+            "Nescafe Tradición 149.5g",
+            4550,
+            drawableResId = R.drawable.img_placement_nescafe
+        ),
+        DummyProduct(
+            "Papel Higiénico 51m 11u",
+            9390,
+            drawableResId = R.drawable.img_placement_papelhigienico
+        ),
+        DummyProduct(
+            "Queso laminado Quilque 251g",
+            3650,
+            drawableResId = R.drawable.img_placement_queso
+        ),
+        DummyProduct(
+            "Nescafe Tradición 148g",
+            3990,
+            drawableResId = R.drawable.img_placement_nescafe
+        ),
+        DummyProduct(
+            "Papel Higiénico 48.9m 12u",
+            8350,
+            drawableResId = R.drawable.img_placement_papelhigienico
+        ),
+        DummyProduct(
+            "Queso laminado Quilque 250.1g",
+            3190,
+            drawableResId = R.drawable.img_placement_queso
+        ),
+    )
+}
---
+++ app/src/main/java/com/example/walmartapp/domain/DummyProduct.kt
@@ -0,0 +1,3 @@
+package com.example.walmartapp.domain
+
+data class DummyProduct(val title: String, val price: Int, val drawableResId: Int)
