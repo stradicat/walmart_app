7962c33583443176bed744320074307de3f4c39a
Dennis Mayr <dmayr.dev@gmail.com>
2024-05-23 10:52:59 -0400

    UI: vista de producto en detalle rediseñada

--- app/src/main/java/com/example/walmartapp/domain/DummyProduct.kt
+++
@@ -1,3 +0,0 @@
-package com.example.walmartapp.domain
-
-data class DummyProduct(val title: String, val price: Int, val drawableResId: Int)
--- app/src/main/java/com/example/walmartapp/view/ProductAdapter.kt
+++
@@ -1,5 +0,0 @@
-package com.example.walmartapp.view
-
-import com.example.walmartapp.domain.DummyData
-
-class ProductAdapter(var products: List<DummyData>)
