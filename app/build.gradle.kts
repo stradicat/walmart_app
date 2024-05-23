0ccf56d9641723279bd9c526afb523dc25a026ac
Dennis Mayr <dmayr.dev@gmail.com>
2024-05-23 00:31:58 -0400

    App: dependencias agregadas

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,6 +15,12 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        vectorDrawables.useSupportLibrary = true
+    }
+
+    buildFeatures {
+        viewBinding = true
     }
 
     buildTypes {
@@ -36,13 +42,22 @@ android {
 }
 
 dependencies {
+    //noinspection UseTomlInstead
 
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.12.0")
+    //noinspection KtxExtensionAvailable
     implementation("androidx.activity:activity:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // Fresco image library
+    implementation("com.facebook.fresco:fresco:3.1.3")
+    implementation("com.facebook.fresco:animated-webp:3.1.3")
+    implementation("com.facebook.fresco:webpsupport:3.1.3")
+    implementation("com.facebook.fresco:imagepipeline-okhttp3:3.1.3")
+    compileOnly("com.facebook.infer.annotation:infer-annotation:0.18.0")
 }
