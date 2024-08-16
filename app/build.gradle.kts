3c9ff9012512c5c48e97344a8549940774a953d0
Dennis Mayr <dmayr.dev@gmail.com>
2024-08-15 20:39:03 -0400

    Dependencias, dependencias, dependencias: downgrade (stable): coroutines; upgrades: AGP, activity-ktx, junit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import com.android.build.gradle.ProguardFiles.getDefaultProguardFile
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -13,6 +15,7 @@ android {
     defaultConfig {
         applicationId = "dev.dmayr.shoppingapp"
         minSdk = 23
+        //noinspection OldTargetApi
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -51,16 +54,17 @@ dependencies {
     //noinspection GradleDependency
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.12.0")
-    //noinspection KtxExtensionAvailable
-    implementation("androidx.activity:activity:1.9.0")
+    implementation("androidx.activity:activity-ktx:1.9.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 
     // Coroutines
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.0")
+    //noinspection GradleDependency
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    //noinspection GradleDependency
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 
     // Lifecycle extensions
     //noinspection GradleDependency
