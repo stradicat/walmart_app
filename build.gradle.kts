7b804b1bbd4ab43a6f93e63bf47941ade6d1666f
Dennis Mayr <dmayr.dev@gmail.com>
2024-08-16 11:50:15 -0400

    Dependency management migrated to TOML format, minor version updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,5 +2,5 @@
 plugins {
     id("com.android.application") version "8.4.2" apply false
     //noinspection GradleDependency
-    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
 }
