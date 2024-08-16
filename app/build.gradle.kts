861d32176404cb4c76b0e7364e065cd7ea08454b
Dennis Mayr <dmayr.dev@gmail.com>
2024-08-16 12:25:12 -0400

    Dependency management migrated to TOML format, minor version updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,6 +16,7 @@ android {
     defaultConfig {
         applicationId = "dev.dmayr.shoppingapp"
         minSdk = 23
+        //noinspection OldTargetApi
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -48,30 +49,39 @@ android {
 }
 
 dependencies {
-    //noinspection UseTomlInstead
-
-    implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.12.0")
-    //noinspection KtxExtensionAvailable
-    implementation("androidx.activity:activity:1.9.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity.ktx)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
 
     // Fresco image library
-    implementation("com.facebook.fresco:fresco:3.1.3")
-    implementation("com.facebook.fresco:animated-webp:3.1.3")
-    implementation("com.facebook.fresco:webpsupport:3.1.3")
-    implementation("com.facebook.fresco:imagepipeline-okhttp3:3.1.3")
-    compileOnly("com.facebook.infer.annotation:infer-annotation:0.18.0")
+    implementation(libs.fresco)
+    implementation(libs.animated.webp)
+    implementation(libs.webpsupport)
+    implementation(libs.imagepipeline.okhttp3)
+    compileOnly(libs.infer.annotation)
 
     // Deserializers:
     // KotlinX Serialization
-    implementation("org.jetbrains.kotlin:kotlin-serialization:1.9.0")
+    implementation(libs.kotlin.serialization)
+
+    // Coroutines
+    //noinspection GradleDependency
+    implementation(libs.kotlinx.coroutines.core)
+    //noinspection GradleDependency
+    implementation(libs.kotlinx.coroutines.android)
+
+    // Lifecycle extensions
+    //noinspection GradleDependency
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    //noinspection GradleDependency
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
 
     // API calls
     // Ktor
-    implementation("io.ktor:ktor-client-android:2.2.3")
+    implementation(libs.ktor.client.android)
 }
