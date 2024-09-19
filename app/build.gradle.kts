bbe6e0d777c1f90dc681064fe828709e38e2a5c0
Dennis Mayr <dmayr.dev@gmail.com>
2024-09-19 13:27:24 -0300

    build.gradle: AGP bump to 8.6.1, Java bump to 17 for compileOptions, lifecycleRuntimeKtx bump to 2.8.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,9 +20,7 @@ android {
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-
         vectorDrawables.useSupportLibrary = true
     }
 
@@ -39,12 +37,14 @@ android {
             )
         }
     }
+
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
+
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 }
 
